316855d5366a53e6c93007984c3732e7155dbe0e
lobomfz <lobomfz@protonmail.com>
2022-08-25 07:25:04 -0300

    we do a little rewriting

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 world's first non-heroku working timer (crazy)
 
-still a lot to be done, early draft stage, adjust websocket url in front.js and settings.json (in seconds) accordingly
+backend rewrite wip
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const tmi = require("tmi.js");
-const express = require("express");
-const cors = require("cors");
 const fs = require("fs");
 const WebSocket = require("ws");
 const io = require("socket.io-client");
+const axios = require("axios");
 
 var socket;
 const port = 3001;
-const portWss = 3002;
+const portWss = 3003;
 const wss = new WebSocket.Server({ port: portWss });
+const client_id = "3qy8w6q7u5u7wamjmggmykmrv3wjj9";
+var db = {};
 
 var settings,
 	timer = 0,
@@ -17,114 +18,20 @@ var settings,
 	sync = false,
 	timeout = 5,
 	forceSync = 30;
-lastMessage = Date.now() / 1000;
 
-function readSettings() {
-	try {
-		var jsonFile = fs.readFileSync("./settings.json").toString();
-	} catch (e) {
-		console.log("Could not load json file!");
-		process.exit();
-	}
-	try {
-		settings = JSON.parse(jsonFile);
-		timer = settings.seconds;
-	} catch (e) {
-		console.log(e);
-		console.log("Failed to parse json");
-		process.exit();
-	}
+function addToTimer(ws, seconds) {
+	amount = parseInt(seconds) || 0;
+	ws.timer += amount;
+	syncTimer(ws);
 }
 
-function addToTimer(type, count = 1, extra) {
-	amount = settings[type] * count;
-	console.log("adding", amount, "seconds to timer");
-	timer += amount;
-	sync = false;
-}
-
-function httpApi() {
-	const app = express();
-	app.use(cors());
-	app.listen(port, () => console.log(`listening on port ${port}`));
-
-	app.post("/addTime", (req, res) => {
-		if (req.headers.password == settings.password) {
-			timer += parseInt(req.headers.seconds);
-			sync = false;
-			console.log("Added " + req.headers.seconds + " seconds to timer");
-			res.send(
-				JSON.stringify({
-					message: "Added " + req.headers.seconds + " seconds to timer",
-				})
-			);
-		} else {
-			res.status(403);
-			res.send(JSON.stringify({ error: "invalid password" }));
-		}
-	});
-
-	app.post("/setTime", (req, res) => {
-		if (req.headers.password == settings.password) {
-			timer = parseInt(req.headers.seconds);
-			sync = false;
-			res.send("Set timer to " + req.headers.seconds + " seconds");
-		} else {
-			res.status(403);
-			res.send(JSON.stringify({ error: "invalid password" }));
-		}
-	});
-
-	app.get("/getTime", (req, res) => {
-		res.send(readableTime);
-	});
-
-	app.post("/setToken", (req, res) => {
-		if (req.headers.password == settings.password) {
-			settings.socket_token = req.headers.sltoken;
-			socket.disconnect();
-			writeSettings();
-			connectStreamlabs();
-		} else {
-			res.status(403);
-			res.send(JSON.stringify({ error: "invalid password" }));
-		}
-	});
-
-	app.post("/setSubTime", (req, res) => {
-		if (req.headers.password == settings.password) {
-			value = parseInt(req.headers.value);
-			settings[1] = value;
-			settings[2] = value * 2;
-			settings[3] = value * 5;
-			writeSettings();
-		} else {
-			res.status(403);
-			res.send(JSON.stringify({ error: "invalid password" }));
-		}
-	});
-
-	app.post("/setDollarValue", (req, res) => {
-		console.log(req.headers.password);
-		if (req.headers.password == settings.password) {
-			value = parseInt(req.headers.value);
-			settings.bits = value / 100;
-			settings.dollar = value;
-			settings.dollar = value;
-			writeSettings();
-		} else {
-			res.status(403);
-			res.send(JSON.stringify({ error: "invalid password" }));
-		}
-	});
-}
-
-function startTMI() {
+function startTMI(ws) {
+	console.log("connecting to", ws.name);
 	const client = new tmi.Client({
 		connection: {
 			reconnect: true,
 		},
-		channels: [settings.channel_name],
+		channels: [ws.name],
 	});
 
 	client.connect();
@@ -133,71 +40,61 @@ function startTMI() {
 		"subgift",
 		(channel, username, streakMonths, recipient, methods, userstate) => {
 			var plan = userstate["msg-param-sub-plan"];
-			addToTimer(plan == "Prime" ? 1 : plan / 1000);
+			var tier = plan == "Prime" ? 1 : plan / 1000;
+			// addToTimer(, ws);
 		}
 	);
 
 	client.on("anongiftpaidupgrade", (channel, username, userstate) => {
 		var plan = userstate["msg-param-sub-plan"];
-		addToTimer(plan == "Prime" ? 1 : plan / 1000);
+		addToTimer(plan == "Prime" ? 1 : plan / 1000, ws);
 	});
 
 	client.on("cheer", (channel, userstate, message) => {
-		addToTimer("bits", parseFloat(userstate["bits"]));
+		addToTimer("bits", parseFloat(userstate["bits"]), ws);
 	});
 
 	client.on(
 		"resub",
 		(channel, username, months, message, userstate, methods) => {
 			var plan = userstate["msg-param-sub-plan"];
-			addToTimer(plan == "Prime" ? 1 : plan / 1000);
+			addToTimer(plan == "Prime" ? 1 : plan / 1000, ws);
 		}
 	);
 
 	client.on("subscription", (channel, username, method, message, userstate) => {
 		var plan = userstate["msg-param-sub-plan"];
-		addToTimer(plan == "Prime" ? 1 : plan);
+		addToTimer(plan == "Prime" ? 1 : plan, ws);
 	});
 }
 
-async function writeSettings() {
-	settings.seconds = timer;
-	await fs.promises.writeFile(
-		"./settings.json",
-		JSON.stringify(settings, null, 4),
-		"UTF-8"
-	);
-}
+function connectStreamlabs(ws) {
+	if (ws.socket) {
+		ws.socket.disconnect();
+	}
 
-function connectStreamlabs() {
-	socket = io.connect(
-		`https://sockets.streamlabs.com?token=${settings.socket_token}`,
+	ws.socket = io.connect(
+		`https://sockets.streamlabs.com?token=${ws.streamlabsToken}`,
 		{
 			reconnect: true,
 			transports: ["websocket"],
 		}
 	);
 
-	socket.on("event", (e) => {
-		console.log(new Date(), "\n", e);
+	ws.socket.on("event", (e) => {
 		if (e.type == "donation") {
-			var amount =
-				settings.currencies[e.message[0].currency] *
-				e.message[0].amount *
-				settings.dollar;
-			console.log("adding", amount, "seconds to timer");
-			timer += amount;
-
-			sync = false;
+			var amount = 1 * e.message[0].amount * ws.dollar;
+			console.log(`adding ${amount} to ${ws.name}`);
+			ws.timer += amount;
+			syncTimer(ws);
 		}
 	});
 }
 
-function lowerTimer() {
-	if (timer > 0) {
-		timer -= 1;
-
-		readableTime =
+function lowerTimer(ws) {
+	if (ws.timer > 0) {
+		ws.timer -= 1;
+		ws.readableTime =
 			Math.floor(timer / 3600) +
 			":" +
 			("0" + (Math.floor(timer / 60) % 60)).slice(-2) +
@@ -206,52 +103,113 @@ function lowerTimer() {
 	}
 }
 
-function connectFront() {
+async function syncTimer(ws) {
+	if (!Number.isInteger(ws.timer)) ws.timer = 0;
+	ws.send(
+		JSON.stringify({
+			time: parseInt(ws.timer),
+			sub: ws.sub,
+			dollar: ws.dollar,
+		})
+	);
+}
+
+async function login(ws, data) {
+	ws.access_token = data.access_token;
+	axios
+		.get(`https://api.twitch.tv/helix/users`, {
+			headers: {
+				Authorization: `Bearer ${ws.access_token}`,
+				"Client-Id": client_id,
+			},
+		})
+		.then((res) => {
+			append = {
+				name: res.data.data[0].login,
+				initialized: true,
+				id: res.data.data[0].id,
+			};
+			Object.assign(ws, append);
+			if (!db[ws.name]) db[ws.name] = ws;
+			getSettings(ws);
+			setInterval(() => {
+				pushToDb(ws);
+			}, 30 * 1000);
+			startTMI(ws);
+		})
+		.catch(function (error) {
+			sendError(ws, "failed to login");
+		});
+}
+
+async function getSettings(ws) {
+	if (db[ws.name]) {
+		ws.timer = db[ws.name].timer || 0;
+		ws.sub = db[ws.name].sub || 60;
+		ws.dollar = db[ws.name].dollar || 12;
+		ws.streamlabsToken = db[ws.name].streamlabsToken || null;
+	}
+	if (ws.streamlabsToken) connectStreamlabs(ws);
+}
+
+async function pushToDb(ws) {
+	db[ws.name] = ws;
+}
+
+async function sendError(ws, message) {
+	ws.send(
+		JSON.stringify({
+			error: message,
+		})
+	);
+}
+
+function main() {
 	wss.on("connection", (ws) => {
-		async function syncTimer() {
-			now = Date.now() / 1000;
-			if (now - lastMessage < timeout) return 0;
-			lastMessage = now;
-			console.log(new Date(), "- syncing:", readableTime);
-			ws.send(
-				JSON.stringify({
-					time: timer,
-					tier_1: settings[1],
-					dollar: settings.dollar,
-				})
-			);
-		}
-
 		setInterval(() => {
-			if (!sync) syncTimer();
-		}, 2000);
-
-		// force sync every n seconds
-		setInterval(() => {
-			syncTimer();
-		}, forceSync * 1000);
+			lowerTimer(ws);
+		}, 1000);
 
 		ws.onmessage = function (event) {
-			console.log(event.data);
-			if (event.data == 1) sync = true;
-			else sync = false;
-			console.log(new Date(), "- synced:", readableTime);
+			try {
+				data = JSON.parse(event.data);
+			} catch (error) {
+				ws.send(
+					JSON.stringify({
+						error: "json error",
+					})
+				);
+				return 0;
+			}
+
+			switch (data.event) {
+				case "login":
+					login(ws, data);
+					break;
+				case "connectStreamlabs":
+					if (!ws.initialized) {
+						sendError(ws, "not initialized");
+						return 0;
+					}
+					ws.streamlabsToken = data.streamlabs_token;
+					connectStreamlabs(ws);
+					break;
+				case "getTime":
+					if (!ws.initialized) {
+						sendError(ws, "not initialized");
+						return 0;
+					}
+					syncTimer(ws);
+					break;
+				case "setSetting":
+					if (!ws.initialized) {
+						sendError(ws, "not initialized");
+						return 0;
+					}
+					ws[data.setting] = data.value;
+			}
 		};
 	});
-}
-function main() {
-	readSettings();
-
-	setInterval(lowerTimer, 1000);
-	setInterval(writeSettings, 10 * 1000);
-
-	httpApi();
-
-	// twitch listeners
-	startTMI();
-	connectStreamlabs();
-
-	connectFront();
 }
 
 main();
